d75a4935e7ce24e2aa79f8bc7c3590cb98730cba
BhawanaSatyal <bhawanasatya@gmail.com>
2022-01-18 20:24:13 +0000

    code updated

--- src/main/java/org/example/BrowserManager.java
+++ src/main/java/org/example/BrowserManager.java
@@ -3,35 +3,80 @@ package org.example;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BrowserManager extends BasePage {
 
     LoadProp loadProp = new LoadProp();
-//    public String browserName = "edge";
-    public String browserName = System.getProperty("browser");
+  //  String browserName = "chrome";
+   String browserName=System.getProperty("browser");
+    Boolean InCloud = false;
+//
+//    public static final String AUTOMATE_USERNAME = "bhawanasatyal_wcbJFy";
+//    public static final String AUTOMATE_ACCESS_KEY = "N5ppFV5AQcwyx8wxYXvG";
+//    public static final String browserStackURL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
-    public  void openBrowser(){
-        if(browserName.equalsIgnoreCase("chrome"))
-        {
-            System.setProperty("webdriver.chrome.driver", "src/test/java/drivers/chromedriver.exe");//System Property for Chrome Driver
-            driver = new ChromeDriver();
+    public static final String AUTOMATE_USERNAME = "bhawanasatyal_wcbJFy";
+    public static final String AUTOMATE_ACCESS_KEY = "N5ppFV5AQcwyx8wxYXvG";
+    public static final String browserStackURL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    public void openBrowser() {
+        if (InCloud) {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("browser", "Edge");
+            caps.setCapability("browser_version", "latest");
+            caps.setCapability("browserstack.local", "false");
+            caps.setCapability("browserstack.selenium_version", "3.14.0");
+//            if (browserName.equalsIgnoreCase("safari")) {
+//                caps.setCapability("os", "OS X");
+//                caps.setCapability("os_version", "Big Sur");
+//                caps.setCapability("browser", "Safari");
+//                caps.setCapability("browser_version", "14.1");
+//                caps.setCapability("browserstack.local", "false");
+//                caps.setCapability("browserstack.selenium_version", "3.141.59");
+//            } else if (browserName.equalsIgnoreCase("chrome")) {
+//                caps.setCapability("os", "Windows");
+//                caps.setCapability("os_version", "7");
+//                caps.setCapability("browser", "Chrome");
+//                caps.setCapability("browser_version", "latest");
+//                caps.setCapability("browserstack.local", "false");
+//                caps.setCapability("browserstack.selenium_version", "3.14.0");
+//            } else if (browserName.equalsIgnoreCase("firefox")) {
+//            }
+
+
+            try {
+                driver = new RemoteWebDriver(new URL(browserStackURL), caps);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                System.out.println(e);
+            }
+
+
+        } else {
+            if (browserName.equalsIgnoreCase("chrome")) {
+                System.setProperty("webdriver.chrome.driver", "src/test/java/drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+            } else if (browserName.equalsIgnoreCase("edge")) {
+                System.setProperty("webdriver.edge.driver", "src/test/java/drivers/msedgedriver.exe");
+                driver = new EdgeDriver();
+            } else {
+                System.out.println("Incorrect Browser");
+            }
+
+
+            driver.manage().window().maximize();
+            driver.get(loadProp.getProperty("url"));
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         }
-        else if(browserName.equalsIgnoreCase("edge")){
-            System.setProperty("webdriver.edge.driver", "src/test/java/drivers/msedgedriver.exe");//System Property for Edge
-            driver = new EdgeDriver();
-
-        }else{
-            System.out.println("Incorrect Browser");
-        }
-
-        driver.manage().window().maximize();// open window fullscreen
-        driver.get(loadProp.getProperty("url"));
-
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);//Applied wait time
-       // driver.get("https://demo.nopcommerce.com/");// launch website
-
 
 
     }
--- src/main/java/org/example/HomePage.java
+++ src/main/java/org/example/HomePage.java
@@ -3,7 +3,8 @@ package org.example;
 import org.openqa.selenium.By;
 
 public class HomePage extends Utils{
-    By registerlink = By.xpath("//a[@class='ico-register']");
+   By registerlink = By.xpath("//a[@class=\"ico-register\"]");
+ //   By registerlink = By.xpath("/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a");
     By _computers = By.xpath("//ul[@class='top-menu notmobile']/Li/a[@href='/computers']");
 
 
